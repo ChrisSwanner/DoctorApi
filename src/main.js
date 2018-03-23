2f5bf7f3b81ef58221d335e23cdbc6f367b732d7
Chris Swanner <cmanswanner@gmail.com>
2018-03-23 16:50:11 -0700

    add address and phone number to lists

--- src/main.js
+++ src/main.js
@@ -2,17 +2,22 @@ import $ from 'jquery';
 import { doctorAjaxCall } from './../src/doctor.js';
 
 var doctorDisplay = function(response) {
-  console.log(response);
+
    for (let i = 0; i < 10; i++) {
-     $('#docDisplay').append("<li>" + "<br>Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number " + response.data[i].practices[i].phones[i].number + "</li>")
+     console.log(response
+    );
+     $('#docDisplay').append("<li>" + "<br>Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number " + response.data[i].practices[i].phones[i].number + "<br>Address: " + response.data[i].practices[i].visit_address.street + "</li>")
   }
 }
 
 
 $(document).ready(function(){
-  $('#docForm').submit(function(event){
+  $('#medForm').submit(function(event){
     event.preventDefault();
   doctorAjaxCall(doctorDisplay);
 
+  $('#docForm').submit(function(event){
+    event.preventDefaault
+  })
   })
 })
