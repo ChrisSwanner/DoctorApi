ecd97f3d0ca13021e84221c5746868a55d280afd
Chris Swanner <cmanswanner@gmail.com>
2018-03-23 14:10:57 -0700

    add functions to display first and last name and phone number

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@ import { doctorAjaxCall } from './../src/doctor.js';
 
 var doctorDisplay = function(response) {
   console.log(response);
-  // for (let i = 0; i < response.length; i++) {
-  //   $('#doctorDisplay').append("<li>" + "<br>Doctor: " + response.data.practices.name)
-  //}
+   for (let i = 0; i < 10; i++) {
+     $('#docDisplay').append("<li>" + "<br>Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number " + response.data[i].practices[i].phones[i].number + "</li>")
+  }
 }
 
 
 $(document).ready(function(){
+  $('#docForm').submit(function(event){
+    event.preventDefault();
   doctorAjaxCall(doctorDisplay);
-  //$('#medIssueForm').submit(function(event){
 
-//  })
+  })
 })
