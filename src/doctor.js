4afe53989f8207ee26580f9a1d09d1e2af46ec82
Chris Swanner <cmanswanner@gmail.com>
2018-03-23 17:26:47 -0700

    add see all docs list

--- src/doctor.js
+++ src/doctor.js
@@ -18,3 +18,20 @@ export function doctorAjaxCall(doctorDisplay, medIssueDisplay) {
       }
   })
 }
+export function doctorLookUp(doctorLookUpDisplay) {
+  $.ajax({
+
+    url: "https://api.betterdoctor.com/2016-03-01/doctors?location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey,
+    type: 'GET',
+    data: {
+      format: 'json'
+    },
+    success: function(doctors) {
+
+        doctorLookUpDisplay(doctors);
+      },
+      error: function() {
+        $('#error').text("error");
+      }
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,23 +1,39 @@
 import $ from 'jquery';
-import { doctorAjaxCall } from './../src/doctor.js';
+import { doctorAjaxCall, doctorLookUp } from './../src/doctor.js';
 
 var doctorDisplay = function(response) {
-
+  console.log(response)
+  if (response.data.length > 0) {
    for (let i = 0; i < 10; i++) {
-     console.log(response
-    );
-     $('#docDisplay').append("<li>" + "<br>Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number " + response.data[i].practices[i].phones[i].number + "<br>Address: " + response.data[i].practices[i].visit_address.street + "</li>")
+     if (response.data[i].practices[i].accepts_new_patients === true) {
+       $('#docDisplay').append("<li>" + "<br>Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number " + response.data[i].practices[i].phones[i].number + "<br>Address: " + response.data[i].practices[i].visit_address.street + "<br>Now Accepting Patients" + "</li>")
+     } else if(response.data[i].practices[i].accepts_new_patients === false) {
+       $('#docDisplay').append("<li>" + "<br>Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number: " + response.data[i].practices[i].phones[i].number + "<br>Address: " + response.data[i].practices[i].visit_address.street + "<br>Not Accepting Patients" + "</li>")
+     }
+   }
+ } else if (response.data.length <= 0) {
+       $('#docDisplay').append("No doctors available")
+     }
   }
-}
+
+  var doctorLookUpDisplay = function(response) {
+    for (let i = 0; i < 20; i++) {
+      $('#docDisplay').append(response.data[i].profile.first_name + " "  + response.data[i].profile.last_name + "<hr>");
+    }
+  }
+
 
 
 $(document).ready(function(){
   $('#medForm').submit(function(event){
+    $('#docDisplay').empty();
     event.preventDefault();
   doctorAjaxCall(doctorDisplay);
+  })
+  $('#docButton').click(function(){
+    event.preventDefault();
+    doctorLookUp(doctorLookUpDisplay);
+  })
 
-  $('#docForm').submit(function(event){
-    event.preventDefaault
-  })
-  })
+
 })
