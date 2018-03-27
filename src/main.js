5be613262c269af154bcf29cdae2b40a1c268923
Guest User <Guest@Epicodus-3A.local>
2018-03-27 15:48:17 -0700

    fix for loop

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ let ApiCall = new ApiCalls();
 let doctorDisplay = function(response) {
   if (response.data.length > 0) {
    for (let i = 0; i < response.data.length; i++) {
-     if (response.data[i].practices[i].accepts_new_patients === true) {
-       $('#docDisplay').append("<li>" + "<br>Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number " + response.data[i].practices[i].phones[i].number + "<br>Address: " + response.data[i].practices[i].visit_address.street + "<br>Now Accepting Patients" + "</li>")
-     } else if(response.data[i].practices[i].accepts_new_patients === false) {
-       $('#docDisplay').append("<li>" + "<br>Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number: " + response.data[i].practices[i].phones[i].number + "<br>Address: " + response.data[i].practices[i].visit_address.street + "<br>Not Accepting Patients" + "</li>")
+     if (response.data[i].practices[0].accepts_new_patients === true) {
+       $('#docDisplay').append("<li>" + "Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number: " + response.data[i].practices[0].phones[0].number + "<br>Address: " + response.data[i].practices[0].visit_address.street + "<br>Now Accepting Patients" + "</li><hr>")
+     } else if(response.data[i].practices[0].accepts_new_patients === false) {
+       $('#docDisplay').append("<li>" + "Doctor: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>Phone Number: " + response.data[i].practices[0].phones[0].number + "<br>Address: " + response.data[i].practices[0].visit_address.street + "<br>Not Accepting Patients" + "</li><hr>")
      }
    }
  } else if (response.data.length <= 0) {
